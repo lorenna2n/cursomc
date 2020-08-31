f497b28828ebc53f05c06be01ca5f95b95922c2f
Paula Lorenna <lorennaaraujo@live.com>
2020-08-31 10:18:12 -0300

    Banco de dados h2 e criação automatica da base de dados

---
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -0,0 +1,92 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// QUINTA: Serializable é exigência da linguagem JAVA.
+
+
+@Entity
+public class Categoria implements Serializable {
+
+	// PRIMEIRO: implementando uma classe de domínio 
+	// atributos básicos da classe (id e nome)
+
+private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	// SEGUNDO: Construtores 
+	// obs: Construtor vazio, instância o objeto mais não jog anada nos atributos 
+	
+	public Categoria () {	
+	}
+	
+	// Contrutor com atributos, recebendo ID E NOME.
+	// criar usando botão direito
+	
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	// TERCEIRO: Criar Gett e Sett
+	// eles são métodos de acesso para os atributos 
+	// criar usando o botão direito 
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// QUARTO: Criar os HashCode e Equals, operações para comparar os objetos por valor.
+	// usar o botão direito - gerador de códigos 
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+}
